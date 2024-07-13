21d0e4df79296dda3b02989a42bf45a1af6ce4ab
Connor Ellis <connor.ellis@churchofjesuschrist.org>
2024-07-13 01:00:03 -0600

    added Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+      colors: {
+        argray: '#414043',
+        current: 'currentColor',
+      },
+  },
+  plugins: [],
+}
+
