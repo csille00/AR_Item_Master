12fb58e8de31aefa7740b6378bf056911acf0f0c
Connor Ellis <ellisconnor88@gmail.com>
2024-07-13 20:02:29 -0600

    added Button.tsx

--- src/Components/SidePanel/sidePanel.tsx
+++ src/Components/SidePanel/sidePanel.tsx
@@ -1,5 +1,3 @@
-import ButtonAndIcon from "../Util/ButtonAndIcon.tsx";
-// import {logout} from "../../assets/LogOut.png"
 import logout from "../../assets/LogOut.png"
 import jewel from "../../assets/Jewel.png"
 import diamond from "../../assets/DiamondRing.png"
@@ -7,12 +5,12 @@ import add from "../../assets/Add.png"
 import settings from "../../assets/Settings.png"
 import logoSrc from "../../assets/Logo.png"
 import {SupabaseClient} from "@supabase/supabase-js";
+import Button from "../Util/Button.tsx";
 
 
 interface SidePanelProps {
     client: SupabaseClient
 }
-
 
 
 const SidePanel: React.FC<SidePanelProps> = ({client}) => {
@@ -28,38 +26,42 @@ const SidePanel: React.FC<SidePanelProps> = ({client}) => {
     return (
         <div className="bg-argray h-full shadow-md p-2">
             <div className="mb-4 mt-4">
-                <img src={logoSrc} alt="Logo" className="w-full" />
+                <img src={logoSrc} alt="Logo" className="w-full"/>
             </div>
             <div className="mb-auto">
-                <ButtonAndIcon
-                    icon = {diamond}
-                    text = {"Jewelry"}
-                    onClick = {buttonClicked}
+                {/*<ButtonAndIcon*/}
+                {/*    icon = {diamond}*/}
+                {/*    text = {"Jewelry"}*/}
+                {/*    onClick = {buttonClicked}*/}
+                {/*/>*/}
+                <Button
+                    icon={diamond}
+                    text={"Jewelry"}
+                    onClick={buttonClicked}/>
+
+                <Button
+                    icon={jewel}
+                    text={"Stones"}
+                    onClick={buttonClicked}
                 />
 
-                <ButtonAndIcon
-                    icon = {jewel}
-                    text = {"Stones"}
-                    onClick = {buttonClicked}
+                <Button
+                    icon={add}
+                    text={"Add Form"}
+                    onClick={buttonClicked}
                 />
 
-                <ButtonAndIcon
-                    icon = {add}
-                    text = {"Add Form"}
-                    onClick = {buttonClicked}
-                />
-
-                <ButtonAndIcon
-                    icon = {settings}
-                    text = {"Admin"}
-                    onClick = {buttonClicked}
+                <Button
+                    icon={settings}
+                    text={"Admin"}
+                    onClick={buttonClicked}
                 />
             </div>
             <div className="mt-auto fixed bottom-0">
-                <ButtonAndIcon
-                   icon={logout}
-                   onClick={logOut}
-                   text={"Log Out"}
+                <Button
+                    icon={logout}
+                    onClick={logOut}
+                    text={"Log Out"}
                 />
             </div>
         </div>
---
+++ src/Components/Util/Button.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface ButtonProps {
+    icon?: string | null;
+    text: string;
+    style?: string | null;
+    onClick: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ icon = null, text, onClick, style = "" }) => {
+
+    const styles = `btn flex items-center w-full py-4 px-6 mb-2 bg-argray text-left hover:outline-none ${style}`;
+
+    return (
+        <a
+            className={styles}
+            onClick={onClick}
+        >
+            {icon && (
+                <img
+                    src={icon}
+                    className="mr-2 h-6 w-6"
+                    alt={text}
+                />
+            )}
+            <span className="p-2 font-extralight text-lightgr hover:text-white hover:font-bold">
+                {text}
+            </span>
+        </a>
+    );
+};
+
+export default Button;
