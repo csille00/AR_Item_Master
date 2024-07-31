e2b9f2f8aba752d711fb6f1260d947c37cdde31e
Connor Ellis <connor.ellis@churchofjesuschrist.org>
2024-07-31 11:15:25 -0600

    add product types enum

--- src/Definitions/enum.ts
+++ src/Definitions/enum.ts
@@ -7,3 +7,18 @@ export enum gender {
     MALE = "male",
     FEMALE = "female"
 }
+
+export enum ProductTypes {
+    ENG = "Engagement Rings",
+    WED = "Wedding Bands",
+    FJR = "Rings",
+    NCK = "Necklaces",
+    EAR = "Earrings",
+    BRA = "Bracelets / Anklets",
+    CHM = "Charms",
+    ACC = "Accessories",
+    GFC = "Gift Cards",
+    FEE = "Service Fees",
+    CON = "Consultations",
+    OTH = "Other"
+}
