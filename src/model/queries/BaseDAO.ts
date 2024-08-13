6285c78fb341cf1cd7c2dd6fa272856dd231652f
Connor Ellis <connor.ellis@churchofjesuschrist.org>
2024-08-13 17:45:47 -0600

    added admin page with basic functionality and no styling

---
+++ src/model/queries/BaseDAO.ts
@@ -0,0 +1,43 @@
+import {Option} from "../../Definitions/DropdownOption.ts";
+import {getClient} from "../getClient.ts";
+
+const client = getClient()
+
+export async function getOptionsFromClient(tableName: string): Promise<Option[] | undefined> {
+    const {data, error} = await client
+        .from(tableName)
+        .select('id,description');
+    if (error) {
+        throw error;
+    }
+    if (data) {
+        const result: Option[] = []
+        data.forEach(item => result.push({id: item.id, description: item.description}))
+        return result
+    }
+}
+
+export async function deleteOption(tableName: string, option: Option): Promise<void> {
+    try {
+        console.log("from base dao: ", tableName, " ", option)
+        await client
+            .from(tableName)
+            .delete()
+            .eq('id', option.id);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export async function updateOption(tableName: string, newOption: Option): Promise<void> {
+    try {
+        await client
+            .from(tableName)
+            .update({description: newOption.description})
+            .eq('id', newOption.id)
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
