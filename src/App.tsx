be6ce4c9c5439d1d817263797598fba698147bf6
Connor Ellis <ellisconnor88@gmail.com>
2024-08-26 11:12:43 -0600

    optimized imports and cleaned up files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
 import Authentication from "./Components/Authentication.tsx";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Jewelry from "./Components/Jewelry.tsx";
 import Layout from "./Components/Layout.tsx";
 import Stone from "./Components/Stone.tsx";
-import Logout from "./Components/Logout.tsx";
-import Admin from "./Components/Admin.tsx";
+import Admin from "./Components/Admin/Admin.tsx";
 import AddJewelryForm from "./Components/Add/AddJewelryForm.tsx";
 import AddStoneForm from "./Components/Add/AddStoneForm.tsx";
 import AuthProtectedRoute from "./Components/AuthProtectedRoute.tsx";
@@ -15,7 +13,7 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<AuthProtectedRoute />}>
+                <Route path="/" element={<AuthProtectedRoute/>}>
                     <Route path="" element={<Layout/>}>
                         <Route index element={<Jewelry/>}/>
                         <Route path="stone" element={<Stone/>}/>
